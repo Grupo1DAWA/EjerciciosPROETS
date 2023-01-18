84a70450d55fabea0c4ed84d7797ddc0567ecbf5
Alejandro <alejandroasp71@gmail.com>
2023-01-18 13:06:10 +0000

    Ejercicios NUM
    Se empieza el ejer1

--- EjerciciosNumeros/EjerciciosNumeros/Program.cs
+++ EjerciciosNumeros/EjerciciosNumeros/Program.cs
@@ -1,11 +1,26 @@
 ﻿using System;
-namespace Ejercicio1
+namespace EjerciciosNumeros
 {
     class Program
     {
         static void Main(string[] args)
         {
+            int opcion = 0;
+            bool salida = false;
+            do
+            {
+                switch (opcion)
+                {
+                    case 1:
+                        Console.WriteLine("Introduce un número para comprobar si es primo");
+                        while (!Int32.TryParse(Console.ReadLine(), out int num))
+                        {
+                            Console.WriteLine("Error, el número debe ser entero");
+                        }
 
+                        break;
+                }
+            } while (!salida);
         }
     }
 }
